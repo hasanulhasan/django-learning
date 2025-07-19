f5c790358ab14199281e5a164a178c6ad4dbdedd
hasanulhasan <hasanlink71@yahoo.com>
2025-07-19 20:04:02 +0600

    django form get and post req done

--- myproject/task/views.py
+++ myproject/task/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from task.forms import TaskForm
+from task.models import Employee, Task
 
 # Create your views here.
 def dashboard(request):
@@ -17,3 +18,31 @@ def home(request):
 
 def contact(request):
     return HttpResponse("<h1 style='color: red'>This is contact page</h1>")
+
+def create_task(request):
+    employees = Employee.objects.all()
+    form = TaskForm(employees=employees)  #for get method
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, employees=employees)
+        if form.is_valid():
+            print(form.cleaned_data)
+            data = form.cleaned_data
+            title = data.get('title')
+            description = data.get('description')
+            due_date = data.get('due_date')
+            assigned_to = data.get('assigned_to')
+            task = Task.objects.create(
+                title=title,
+                description=description,
+                due_date=due_date,
+            )
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id=emp_id)
+                task.assigned_to.add(employee)
+            # task.save()
+            return HttpResponse("<h1 style='color: green'>Task created successfully</h1>")
+    context = {
+        'form': form
+    }
+    return render(request, "task_form.html", context)
