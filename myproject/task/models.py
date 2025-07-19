38d462a83f208f343c732b15a6931ebf91baec0e
hasanulhasan <hasanlink71@yahoo.com>
2025-07-19 16:03:36 +0600

    database table created

--- myproject/task/models.py
+++ myproject/task/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
+class Employee(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+
 # Create your models here.
 class Task(models.Model):
     project = models.ForeignKey('Project', on_delete=models.CASCADE, default = 1)
+    assigned_to = models.ManyToManyField(Employee)
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    #taskdetils_set = models.OneToOneField('TaskDetails', on_delete=models.CASCADE, null=True, blank=True)
 
 class TaskDetails(models.Model):
         HIGH = 'H'
@@ -19,7 +25,11 @@ class TaskDetails(models.Model):
             (MEDIUM, 'Medium'),
             (HIGH, 'High'),
         )
-        task = models.OneToOneField(Task, on_delete=models.CASCADE)
+        task = models.OneToOneField(
+             Task, 
+             on_delete=models.CASCADE,
+             related_name='details',
+        )
         assigned_to = models.CharField(max_length=100)
         priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
 
